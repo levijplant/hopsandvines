f04f65f7f025c26b876f01cdc897812503dbddb5
Levi Plant <levijplant@gmail.com>
2020-10-07 20:25:12 -0700

    new resources added

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Header } from 'semantic-ui-react';
+import { Container, Header, Image } from 'semantic-ui-react';
 import './Hero.css';
 
 const Hero = () => {
@@ -7,7 +7,15 @@ const Hero = () => {
     return (
 
         <Container id="my-hero" fluid>
-
+            <div className='address'>
+                <address>
+                    20633 Gas Point Rd, Suite A1<br/>
+                    Cottonwood, CA 96002
+                </address>
+                <Image src='./images/map-pin.svg' alt='Map Pin'>Get Directions</Image>
+                <br/>
+                <a href="tel:5303482308">(530) 348-2308</a>
+            </div>
         </Container>
 
     )
