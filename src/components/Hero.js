7381dff4105493e93cf021bc9affafb94d91b670
Levi Plant <levijplant@gmail.com>
2020-10-14 21:45:33 -0700

    a little more done, not much

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Header, Image } from 'semantic-ui-react';
+import { Container, Grid } from 'semantic-ui-react';
 import './Hero.css';
 
 const Hero = () => {
@@ -7,15 +7,64 @@ const Hero = () => {
     return (
 
         <Container id="my-hero" fluid>
-            <div className='address'>
-                <address>
-                    20633 Gas Point Rd, Suite A1<br/>
+            <section className='main-header'>
+                <div className='address'>
+                    <address>
+                        20633 Gas Point Rd, Suite A1<br />
                     Cottonwood, CA 96002
-                </address>
-                <Image src='./images/map-pin.svg' alt='Map Pin'>Get Directions</Image>
-                <br/>
-                <a href="tel:5303482308">(530) 348-2308</a>
-            </div>
+                        </address>
+                    <div className='directions'>
+                        <img
+                            className='map-pin-icon'
+                            src={require('./images/map-pin.svg')}
+                            alt='Map Pin' />
+                        <span>Get Directions</span>
+                    </div>
+                    <div className='phone'>
+                        <img
+                            className='phone-icon'
+                            src={require('./images/phone-button.svg')}
+                            alt='Phone' />
+                        <a href="tel:5303482308">(530) 348-2308</a>
+                    </div>
+                </div>
+                <div className='logo'>
+                    <img
+                        className='hops-and-vines-logo'
+                        src={require('./images/hops-and-vines-logo.svg')}
+                        alt='Hops and Vines Logo' />
+                </div>
+                <div className='social'>
+                    <p>Connect with us!</p>
+                    <div>
+                        <img
+                            className='facebook-logo'
+                            src={require('./images/facebook-logo-icon.svg')}
+                            alt='Facebook Logo' />
+                        <img
+                            className='untappd-logo'
+                            src={require('./images/untappd-logo-icon.svg')}
+                            alt='Untappd Logo' />
+                        <img
+                            className='yelp-logo'
+                            src={require('./images/yelp-logo-icon.svg')}
+                            alt='Yelp Logo' />
+                    </div>
+                </div>
+            </section>
+            <section className='welcome-section'>
+                <h1 className='welcome-message'>Welcome to Hops & Vines!</h1>
+                <div className='glass-grapes'>
+                    <img
+                        className='glass'
+                        src={require('./images/beers.svg')}
+                        alt='Beer Glass' />
+                    <img
+                        className='grapes'
+                        src={require('./images/grapes.svg')}
+                        alt='Grapes!' />
+                </div>
+            </section>
         </Container>
 
     )
