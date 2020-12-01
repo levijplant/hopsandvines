0e747262927d43269d5b1e47199ce7d218e674f6
Levi Plant <levijplant@gmail.com>
2020-11-30 21:30:29 -0800

    finishing touches on location/hours and menu pages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Segment } from 'semantic-ui-react'
 import './Footer.css';
 
 const Footer = () => {
--- src/components/LocationHours.js
+++ src/components/LocationHours.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Container, Grid } from 'semantic-ui-react'
 import './LocationHours.css';
-
 const LocationHours = () => {
-    
-    return(
+    return (
         <Container className='main-container'>
             <Grid>
                 <Grid.Row>
@@ -30,20 +28,11 @@ const LocationHours = () => {
                         </div>
                     </Grid.Column>
                     <Grid.Column className='map-container'>
-                        <iframe className='map' title='mapLocation' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3038.9096786563464!2d-122.29094098461063!3d40.38869437936832!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54d2c5b9ef993f3f%3A0xfc26cb997090ad3!2sHops%20%26%20Vines!5e0!3m2!1sen!2sus!4v1606515373673!5m2!1sen!2sus" /> 
+                        <iframe className='map' title='mapLocation' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3038.9096786563464!2d-122.29094098461063!3d40.38869437936832!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54d2c5b9ef993f3f%3A0xfc26cb997090ad3!2sHops%20%26%20Vines!5e0!3m2!1sen!2sus!4v1606515373673!5m2!1sen!2sus" />
                     </Grid.Column>
                 </Grid.Row>
-                <Grid.Row style={{justifyContent: 'center'}}>
-                    <div className='logo'>
-                        <img
-                            className='hop-image'
-                            src={require('./images/hop.jpg')}
-                            alt='Hops and Vines Logo' />
-                    </div>
-                </Grid.Row>
             </Grid>
-        </Container> 
+        </Container>
     )
 }
-
 export default LocationHours;
