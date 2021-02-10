115d657603230c97c4c25c410f0a276a2f30e775
Garrett Wren <gwren19@gmail.com>
2021-02-09 20:34:22 -0800

    Made progress on changing NavBar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Image from 'react-bootstrap/Image';
 import './Footer.css';
 
 const Footer = () => {
@@ -7,9 +6,15 @@ const Footer = () => {
     return (
         <div id='footer' fluid>
             <div className='mobile-social'>
-                <a href='https://www.facebook.com/Hops-Vines-392847171276786' target='_blank' rel='noopener noreferrer'><Image style={{ color: 'black' }} inverted name='facebook official' size='big' link /></a>
-                <a href='https://untappd.com/v/hops-and-vines/9014721' target='_blank' rel='noopener noreferrer'><Image style={{ color: 'black' }} inverted name='untappd' size='big' link /></a>
-                <a href='https://www.yelp.com/biz/hops-and-vines-cottonwood-2' target='_blank' rel='noopener noreferrer'><Image style={{ color: 'black' }} inverted name='yelp' size='big' link /></a>
+                <a href='https://www.facebook.com/Hops-Vines-392847171276786' target='_blank' rel='noopener noreferrer'>
+                    <img src={require("./images/facebook-logo-icon.svg")} alt='facebook logo'/>
+                </a>
+                <a href='https://untappd.com/v/hops-and-vines/9014721' target='_blank' rel='noopener noreferrer'>
+                    <img src={require("./images/untappd-logo-icon.svg")} alt='untappd logo' />
+                </a>
+                <a href='https://www.yelp.com/biz/hops-and-vines-cottonwood-2' target='_blank' rel='noopener noreferrer'>
+                    <img src={require("./images/yelp-logo-icon.svg")} alt='yelp logo'/>
+                </a>
             </div>
         </div>
     )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,24 +2,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import "./Navbar.css";
 
 const NavBar = () => {
-  // const [state, setState] = useState({ activeItem: "" });
-  // const history = useHistory();
-
-  // const handleItemClick = (e, { href }) => {
-  //   e.preventDefault();
-  //   setState({ activeItem: href });
-  //   let path = `/${href}`;
-  //   history.push(path);
-  // };
 
   return (
-    <Navbar collapseOnSelect expand="lg">
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
+    <Navbar>
         <Nav className="mr-auto">
+          <NavDropdown className="dropdown" title={<FontAwesomeIcon icon={faBars} />} id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          </NavDropdown>
           <Navbar.Text>
             <Link to="/" id="navbar-link">
               Home
@@ -39,100 +38,24 @@ const NavBar = () => {
         <Nav>
           <Navbar.Brand href="https://www.facebook.com/Hops-Vines-392847171276786/" target="_blank" rel='noreferrer noopener'>
             <img
-              src={require("./images/facebook-logo-icon.svg")}
+              src={require("./images/facebook-logo-icon-green.svg")}
               alt='facebook logo'
             />
           </Navbar.Brand>
           <Navbar.Brand href="https://untappd.com/v/hops-and-vines/9014721" target="_blank" rel='noreferrer noopener'>
             <img
-              src={require("./images/untappd-logo-icon.svg")}
+              src={require("./images/untappd-logo-icon-green.svg")}
               alt='untappd logo'
             />
           </Navbar.Brand>
           <Navbar.Brand href="https://www.yelp.com/biz/hops-and-vines-cottonwood-2" target="_blank" rel='noreferrer noopener'>
             <img
-              src={require("./images/yelp-logo-icon.svg")}
+              src={require("./images/yelp-logo-icon-green.svg")}
               alt='yelp logo'
             />
           </Navbar.Brand>
         </Nav>
-        </Navbar.Collapse>
       </Navbar>
-
-    // <>
-    //     <Segment
-    //         className='nav-bar'
-    //         inverted>
-    //         <Menu inverted pointing secondary>
-    //             <Menu.Item
-    //                 link
-    //                 name=''
-    //                 active={state.activeItem === '/'}
-    //                 onClick={handleItemClick}
-    //             >
-    //                 <Icon style={{ color: '#C1D72D' }} inverted name='home' size='large' link />
-    //             </Menu.Item>
-    //             <Menu.Item
-    //                 link
-    //                 name='locationhours'
-    //                 active={state.activeItem === '/locationhours'}
-    //                 onClick={handleItemClick}
-    //             >
-    //                 Location/Hours
-    //             </Menu.Item>
-    //             <Menu.Item
-    //                 link
-    //                 name='menu'
-    //                 active={state.activeItem === '/menu'}
-    //                 onClick={handleItemClick}
-    //             />
-    //             <Menu.Item
-    //                 name='about'
-    //                 link
-    //             />
-    //             <Menu.Item
-    //                 className='normal-social'
-    //                 position='right'>
-    //                 <a href='https://www.facebook.com/Hops-Vines-392847171276786' target='_blank' rel='noopener noreferrer'><Icon style={{ color: '#C1D72D' }} inverted name='facebook official' size='large' link /></a>
-    //                 <a href='https://untappd.com/v/hops-and-vines/9014721' target='_blank' rel='noopener noreferrer'><Icon style={{ color: '#C1D72D' }} inverted name='untappd' size='large' link /></a>
-    //                 <a href='https://www.yelp.com/biz/hops-and-vines-cottonwood-2' target='_blank' rel='noopener noreferrer'><Icon style={{ color: '#C1D72D' }} inverted name='yelp' size='large' link /></a>
-    //             </Menu.Item>
-    //         </Menu>
-    //     </Segment>
-
-    //     <Segment
-    //         className='nav-bar-mobile'
-    //         inverted>
-    //         <Menu inverted>
-    //             <Dropdown item icon='content' simple style={{ color: '#C1D72D' }}>
-    //                 <Dropdown.Menu>
-    //                     <Dropdown.Item
-    //                         name=''
-    //                         active={state.activeItem === '/'}
-    //                         onClick={handleItemClick}>
-    //                         <Icon name='home' />
-    //                     </Dropdown.Item>
-    //                     <Dropdown.Item
-    //                         name='locationhours'
-    //                         active={state.activeItem === '/locationhours'}
-    //                         onClick={handleItemClick}
-    //                     >Location/Hours
-    //                     </Dropdown.Item>
-    //                     <Dropdown.Item
-    //                         name='menu'
-    //                         active={state.activeItem === '/menu'}
-    //                         onClick={handleItemClick}
-    //                     >Menu</Dropdown.Item>
-    //                     <Dropdown.Item
-    //                         name='menu'
-    //                         active={state.activeItem === '/menu'}
-    //                         onClick={handleItemClick}
-    //                     >About</Dropdown.Item>
-    //                 </Dropdown.Menu>
-    //             </Dropdown>
-    //         </Menu>
-    //     </Segment>
-    // </>
   );
 };
 
