12fe7399a53b386650e858ba5d53551797f169b4
Levi Plant <levijplant@gmail.com>
2021-02-11 19:53:14 -0800

    media queries and added dropdown links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,54 +9,74 @@ import "./Navbar.css";
 
 const NavBar = () => {
 
-  return (
-    <Navbar>
-        <Nav className="mr-auto">
-          <NavDropdown className="dropdown" title={<FontAwesomeIcon icon={faBars} />} id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-          </NavDropdown>
-          <Navbar.Text>
-            <Link to="/" id="navbar-link">
-              Home
-            </Link>
-          </Navbar.Text>
-          <Navbar.Text>
-            <Link to="/locationhours" id="navbar-link">
-              Location/Hours
-            </Link>
-          </Navbar.Text>
-          <Navbar.Text>
-            <Link to="/menu" id="navbar-link">
-              Menu
-            </Link>
-          </Navbar.Text>
-        </Nav>
-        <Nav>
-          <Navbar.Brand href="https://www.facebook.com/Hops-Vines-392847171276786/" target="_blank" rel='noreferrer noopener'>
-            <img
-              src={require("./images/facebook-logo-icon-green.svg")}
-              alt='facebook logo'
-            />
-          </Navbar.Brand>
-          <Navbar.Brand href="https://untappd.com/v/hops-and-vines/9014721" target="_blank" rel='noreferrer noopener'>
-            <img
-              src={require("./images/untappd-logo-icon-green.svg")}
-              alt='untappd logo'
-            />
-          </Navbar.Brand>
-          <Navbar.Brand href="https://www.yelp.com/biz/hops-and-vines-cottonwood-2" target="_blank" rel='noreferrer noopener'>
-            <img
-              src={require("./images/yelp-logo-icon-green.svg")}
-              alt='yelp logo'
-            />
-          </Navbar.Brand>
-        </Nav>
-      </Navbar>
-  );
+    return (
+        <Navbar>
+            <Nav className="mr-auto">
+                <NavDropdown className="dropdown" title={<FontAwesomeIcon icon={faBars} />} id="basic-nav-dropdown">
+                    <NavDropdown.Item>
+                        <Link to="/" className='dropdown-link'>
+                            Home
+                        </Link>
+                    </NavDropdown.Item>
+                    <NavDropdown.Item>
+                        <Link to="/locationhours" className='dropdown-link'>
+                            Location/Hours
+                        </Link>
+                    </NavDropdown.Item>
+                    <NavDropdown.Item>
+                        <Link to="/menu" className='dropdown-link'>
+                            Menu
+                        </Link>
+                    </NavDropdown.Item>
+                    <NavDropdown.Item>
+                        <Link to="/" className='dropdown-link'>
+                            About
+                        </Link>
+                    </NavDropdown.Item>
+                </NavDropdown>
+                <Navbar.Text className='nav-bar-nonmobile'>
+                    <Link to="/" id="navbar-link">
+                        Home
+                        </Link>
+                </Navbar.Text>
+                <Navbar.Text className='nav-bar-nonmobile'>
+                    <Link to="/locationhours" id="navbar-link">
+                        Location/Hours
+                    </Link>
+                </Navbar.Text>
+                <Navbar.Text className='nav-bar-nonmobile'>
+                    <Link to="/menu" id="navbar-link">
+                        Menu
+                    </Link>
+                </Navbar.Text>
+                <Navbar.Text className='nav-bar-nonmobile'>
+                    <Link to="/" id="navbar-link">
+                        About
+                    </Link>
+                </Navbar.Text>
+            </Nav>
+            <Nav className='nav-bar-nonmobile'>
+                <Navbar.Brand href="https://www.facebook.com/Hops-Vines-392847171276786/" target="_blank" rel='noreferrer noopener'>
+                    <img
+                        src={require("./images/facebook-logo-icon-green.svg")}
+                        alt='facebook logo'
+                    />
+                </Navbar.Brand>
+                <Navbar.Brand href="https://untappd.com/v/hops-and-vines/9014721" target="_blank" rel='noreferrer noopener'>
+                    <img
+                        src={require("./images/untappd-logo-icon-green.svg")}
+                        alt='untappd logo'
+                    />
+                </Navbar.Brand>
+                <Navbar.Brand href="https://www.yelp.com/biz/hops-and-vines-cottonwood-2" target="_blank" rel='noreferrer noopener'>
+                    <img
+                        src={require("./images/yelp-logo-icon-green.svg")}
+                        alt='yelp logo'
+                    />
+                </Navbar.Brand>
+            </Nav>
+        </Navbar>
+    );
 };
 
 export default NavBar;
--- src/components/Untappd.js
+++ src/components/Untappd.js
@@ -4,28 +4,28 @@ import Image from "react-bootstrap/Image";
 import "./Untappd.css";
 
 const Untappd = () => {
-  return (
-    <Container id="untappd-div">
-      <h2 className="rotation">Beer Rotation</h2>
-      <h4 className="visit-untappd">
-        We rotate our beers daily. Visit Untappd to see what's new today.
-      </h4>
-      <a
-        href="https://untappd.com/v/hops-and-vines/9014721"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <button className="view-untappd">
-          View Beers on Untappd
-          <Image
-            name="arrow right"
-            size="large"
-            style={{ paddingLeft: "10px" }}
-          />
-        </button>
-      </a>
-    </Container>
-  );
+    return (
+        <Container id="untappd-div">
+            <h2 className="rotation">Beer Rotation</h2>
+            <h4 className="visit-untappd">
+                We rotate our beers daily. Visit Untappd to see what's new today.
+            </h4>
+            <a
+                href="https://untappd.com/v/hops-and-vines/9014721"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <button className="view-untappd">
+                    View Beers on Untappd
+                        <Image
+                            name="arrow right"
+                            size="large"
+                            style={{ paddingLeft: "10px" }}
+                    />
+                </button>
+            </a>
+        </Container>
+    );
 };
 
 export default Untappd;
